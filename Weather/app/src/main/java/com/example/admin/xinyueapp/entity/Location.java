b328adee515c1096158446c3c723de5fc1926a3b
孙骄 <xzzzf.dsx@gmail.com>
2019-01-14 16:29:36 +0800

    Location.java

--- Weather/app/src/main/java/com/example/admin/xinyueapp/entity/Location.java
+++ Weather/app/src/main/java/com/example/admin/xinyueapp/entity/Location.java
@@ -5,9 +5,13 @@ import java.util.HashMap;
 public class Location {
     private String mLocation;
     private String mAdmin;
+    private String mCity;
+    private String mProvince;
     private String mCid;
     public Location(String location , String city , String province , String cid){
         mLocation = location;
+        mCity = city;
+        mProvince = province;
         if(province.equals(" "))
             mAdmin=city;
         else
@@ -17,6 +21,9 @@ public class Location {
 
     public String getLocation(){
         return mLocation;
+    }
+    public String getCity(){
+        return mCity;
     }
 
     public String getCid() {
